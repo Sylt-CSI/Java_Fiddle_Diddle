304ab42a072e52c4bcdfb954cd83f9f1ab8e916d
Tyr-Lys <kaasdata@gmail.com>
2020-04-02 12:09:27 +0200

    derp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(args));
-        // pass derp
+        // pass derp lala
     }
 }
 
