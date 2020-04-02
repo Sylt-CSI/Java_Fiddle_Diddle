25dfee7cb4ca368a39a4843608fd75ebe431667b
Tyr-Lys <kaasdata@gmail.com>
2020-04-02 11:42:36 +0200

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
+
+
+
